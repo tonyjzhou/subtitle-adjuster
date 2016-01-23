1078e87281036871f77cc285457629e3fe2a3400
Tony ZHOU <zjt1114@gmail.com>
2016-01-23 08:01:19 +0800

    Refacto: rename test case

--- tests/test_srt_adjuster.py
+++ tests/test_srt_adjuster.py
@@ -4,11 +4,11 @@ from adjuster.srt_adjuster import make_new_filename
 
 
 class TestSrtAdjuster(unittest.TestCase):
-    def test_adjust_make_new_filename_normalFile_addNewInBase(self):
+    def test_make_new_filename_normalFile_addNewInBase(self):
         filename = "normal.srt"
         self.assertEqual("normal_new.srt", make_new_filename(filename))
 
-    def test_adjust_make_new_filename_fileWithoutExtension_addNewInBase(self):
+    def test_make_new_filename_fileWithoutExtension_addNewInBase(self):
         filename = "normal"
         self.assertEqual("normal_new", make_new_filename(filename))
 
