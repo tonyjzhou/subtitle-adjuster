e17ab1d4fbfdc1468e61a172d2d53e56c615b42f
Tony ZHOU <zjt1114@gmail.com>
2017-01-29 16:04:03 -0800

    CHANGE: Print error messages in a more readable way

--- README.md
+++ README.md
@@ -11,6 +11,7 @@ With this simple program, you can adjust the subtitle show up time by seconds.
     * files
     * line_adjuster
 * Provide prompting for user to input the output path he want the new srt to be
-    * When no output path provided, it will be defaulted to srt
+    * When no output path provided, it will be defaulted to the same path of the original file
     * When the output path is the same as the current srt path, the new srt will have the "_new" addendum
     * When the output path is different than the current srt path, no "_new" will be added to the new file name
+    * Provide a way quit
--- srt_adjuster.py
+++ srt_adjuster.py
@@ -24,31 +24,31 @@ def ask_user_input():
 
 def ask_valid_seconds():
     while True:
-        value = input('Enter seconds to add to or remove from (negative number) original srt (e.g. 1 or -1): ')
+        value = input('Enter seconds to add to or remove from original srt (e.g. 1 or -1): ')
         try:
             return int(value)
         except ValueError:
-            print('Invalid integer value: "%s"\nPlease try again ...\n' % value)
+            print('\n\tInvalid integer value: "%s"\n\tPlease try again ...\n' % value)
 
 
 def ask_valid_new_path_name():
     while True:
-        path_name = input('Enter path for the new srt file (e.g. /Users/tonyzhou/Movies): ')
+        path_name = input('Enter path for the new srt (e.g. /Users/tonyzhou/Movies): ')
 
         if os.path.isdir(path_name):
             return path_name
         else:
-            print('Invalid new path: "%s"\nPlease try again ...\n' % path_name)
+            print('\n\tInvalid new path: "%s"\n\tPlease try again ...\n' % path_name)
 
 
 def ask_valid_original_file_name():
     while True:
-        file_name = input('Enter full srt original file path (e.g. /Users/tonyzhou/Movies/abc.srt): ')
+        file_name = input('Enter full file name for original srt (e.g. /Users/tonyzhou/Movies/abc.srt): ')
 
         if os.path.isfile(file_name) and is_valid_file_type(file_name):
             return file_name
         else:
-            print('Invalid original file: "%s"\n' % file_name)
+            print('\n\tInvalid original file: "%s"\n\tPlease try again ...\n' % file_name)
 
 
 def main():
